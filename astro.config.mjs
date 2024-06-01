d6c334962480c3daed2d2417afa94d48ac5de6ae
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2024-06-01 19:14:09 +0300

    Fix site address

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ozanmuyes.github.io",
+  site: "https://ozanmuyes.com",
   integrations: [tailwind()],
 });
